3300d056a55080bec8f3a3f1d3c6d71ce7dec030
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-07-29 20:08:23 +0300

    Find duplicate in array exe done

--- array/find_duplicate_in_array.go
+++ array/find_duplicate_in_array.go
@@ -2,6 +2,24 @@ package array
 
 // Given a list of integers (1,2,...,n), find a duplicate number in O(n) time.
 
+// O(n) time
+// O(n) space
+//
+// create a hashmap
+// check if num exists, if it does, we have found the duplicate
+// if not, add it to hashmap
+// if duplicate is not detected, return -1
+
 func FindDuplicate(list []int) int {
-	return 69
+	hashmap := make(map[int]bool)
+
+	for i := 0; i < len(list); i++ {
+		if _, ok := hashmap[list[i]]; ok {
+			return list[i]
+		} else {
+			hashmap[list[i]] = true
+		}
+	}
+
+	return -1
 }
